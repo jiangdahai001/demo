150d0f3a32bd068376f2b7d1fb58688606754fbd
jiangdahai001 <34437461+jiangdahai001@users.noreply.github.com>
2019-05-30 20:22:48 +0800

    Update Tmain.java

--- src/main/java/Tmain.java
+++ src/main/java/Tmain.java
@@ -2,4 +2,7 @@ public class Tmain {
     public static void main(String[] args) {
         System.out.print("hello world");
     }
+    public static void sayHello(String name) {
+        System.out.print(name);
+    }
 }
